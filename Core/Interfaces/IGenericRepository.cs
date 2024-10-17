ccc766862b261e045dd06f7083c7ef2d84a3de68
Kumaran98 <rajeswaranilankumaran@gmail.com>
2024-10-17 18:44:06 +0530

    End of Section 4

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Data.Common;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IGenericRepository<T> where T: BaseEntity
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T?> GetEntityWothSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<TResult?> GetEntityWothSpec<TResult>(ISpecification<T, TResult> spec);
+    Task<IReadOnlyList<TResult>> ListAsync<TResult>(ISpecification<T, TResult> spec);
+    
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<bool> SaveAllAsync();
+    bool Exists(int id);
+
+}

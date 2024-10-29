9d64cd339abcc2702c63bef2512ef153b72ae346
Kumaran98 <rajeswaranilankumaran@gmail.com>
2024-10-29 16:54:04 +0530

    End of Section 13

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Product } from '../../../shared/models/products';
 import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-product-item',
@@ -19,5 +20,5 @@ import { RouterLink } from '@angular/router';
 })
 export class ProductItemComponent {
   @Input() product?: Product;
-
+  cartService = inject(CartService);
 }
